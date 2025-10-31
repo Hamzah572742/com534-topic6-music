b46d694ed2ed5fcdf9baaca31c00ceb6505092c6
Hamzah572742 <0hussh59@solent.ac.uk>
2025-10-31 11:00:17 +0000

    Enhance play method to display music details

--- src/RecordPlayer.kt
+++ src/RecordPlayer.kt
@@ -1,3 +1,17 @@
 class RecordPlayer {
-    
+
+    fun play(music: Music) {
+        println("Now playing...")
+
+        // Show total playing time
+        println("Total playing time: ${music.getPlayingTime()} seconds")
+
+        // Show all songs
+        println("Songs:")
+        music.getAllSongs().forEach { song ->
+            println(" - ${song.title} by ${song.artist} (${song.playingTime} seconds)")
+        }
+
+        println("Playback finished.\n")
+    }
 }
