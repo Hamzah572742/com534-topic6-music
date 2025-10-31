8ea62e1fbc66581aa05ec2657253aee80d6c60e9
Hamzah Hussain <0hussh59@solent.ac.uk>
2025-10-31 10:31:52 +0000

    Added methods for playing time

--- src/Album.kt
+++ src/Album.kt
@@ -1,3 +1,16 @@
-class Album(val songs: List<Song>) {
+class Album(val songs: List<Song>):Music {
+    fun findSongsByArtist(searchArtist: String): List<Song> {
+        return songs.filter { it.title == searchArtist }
+    }
 
-}
+    fun getPlayingTime(): Double {
+            return songs.sumOf { it.playingTime }
+
+        }
+
+
+
+        fun allPlayingTime(): List<Song> {
+            return songs
+        }
+    }
