d881405d9f5d0e463b0392e2d8269e05675a6245
Hamzah572742 <0hussh59@solent.ac.uk>
2025-10-31 10:57:45 +0000

    Implement RecordPlayer class for music playback
    Added RecordPlayer class to handle music playback.

--- src/Album.kt
+++ src/Album.kt
@@ -14,3 +14,23 @@ class Album(val songs: List<Song>):Music {
             return songs
         }
     }
+
+
+
+class RecordPlayer {
+
+    fun play(music: Music) {
+        println("Now playing...")
+
+        // Show total playing time
+        println("Total playing time: ${music.getPlayingTime()} seconds")
+
+        // Show all songs
+        println("Songs:")
+        music.getAllSongs().forEach { song ->
+            println(" - ${song.title} by ${song.artist} (${song.playingTime} seconds)")
+        }
+
+        println("Playback finished.\n")
+    }
+}
